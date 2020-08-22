c53d9fa38cc4ac59f23e46b2b0cbf9f5c3ad8fd5
Satu <kttth0320@gmail.com>
2020-08-22 17:15:37 +0900

    PHP事前課題①の初回PRです。

--- html/php-pre-challenge1/index.php
+++ html/php-pre-challenge1/index.php
@@ -1,6 +1,14 @@
 <?php
 for ($i = 1; $i <= 100; $i++) {
     //ここからコードを書く
-    aaaaaa
-    echo $i;
+    if(($i % 3 === 0) && ($i % 5 === 0)){
+        print('3と5の倍数<br />');
+    } elseif($i % 5 === 0){
+        print('5の倍数<br />');
+    } elseif($i % 3 === 0){
+        print('3の倍数<br />');
+    } else {
+        print($i.'<br />');
+    }
 }
+?>
