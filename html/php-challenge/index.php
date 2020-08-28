3018dfb3fe162cca415c8db37f89245824959dbd
Satu <kttth0320@gmail.com>
2020-08-28 13:06:01 +0900

    fontawesomeの挿入

--- html/php-challenge/index.php
+++ html/php-challenge/index.php
@@ -74,7 +74,7 @@ function makeLink($value) {
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>ひとこと掲示板</title>
-
+	<script src="https://kit.fontawesome.com/58440588d4.js" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="style.css" />
 </head>
 
@@ -106,24 +106,27 @@ foreach ($posts as $post):
     <div class="msg">
     <img src="member_picture/<?php echo h($post['picture']); ?>" width="48" height="48" alt="<?php echo h($post['name']); ?>" />
     <p><?php echo makeLink(h($post['message'])); ?><span class="name">（<?php echo h($post['name']); ?>）</span>[<a href="index.php?res=<?php echo h($post['id']); ?>">Re</a>]</p>
-    <p class="day"><a href="view.php?id=<?php echo h($post['id']); ?>"><?php echo h($post['created']); ?></a>
+    <!-- fontawesomeの入れ方を調べる。多分普通にhtmlのheadにfontawesome読み込ませて、その後コードコピペして貼り付けるだけだった気がする　ちなみにリツイートのやつはコレ <i class="fas fa-retweet"></i>  -->
+	<i class="fas fa-retweet"></i>
+	<i class="far fa-thumbs-up"></i>
+	<p class="day"><a href="view.php?id=<?php echo h($post['id']); ?>"><?php echo h($post['created']); ?></a>
 		<?php
-if ($post['reply_post_id'] > 0):
-?>
-<a href="view.php?id=<?php echo
-h($post['reply_post_id']); ?>">
-返信元のメッセージ</a>
-<?php
-endif;
-?>
-<?php
-if ($_SESSION['id'] == $post['member_id']):
-?>
-[<a href="delete.php?id=<?php echo h($post['id']); ?>"
-style="color: #F33;">削除</a>]
-<?php
-endif;
-?>
+		if ($post['reply_post_id'] > 0):
+		?>
+		<a href="view.php?id=<?php echo
+		h($post['reply_post_id']); ?>">
+		返信元のメッセージ</a>
+		<?php
+		endif;
+		?>
+		<?php
+		if ($_SESSION['id'] == $post['member_id']):
+		?>
+		[<a href="delete.php?id=<?php echo h($post['id']); ?>"
+		style="color: #F33;">削除</a>]
+		<?php
+		endif;
+		?>
     </p>
     </div>
 <?php
